6958c220603e8b1321e4d50ab88e5851eb740303
Michael (Daichi) Yanagi <6637846+dyanagi@users.noreply.github.com>
2019-12-25 19:07:18 -0800

    Arrow Function

---
+++ basic/arrow-function.js
@@ -0,0 +1,14 @@
+const addWithBlock = (a, b) => {
+  return a + b
+}
+const add = (a, b) => a + b
+const addOne = a => a + 1
+const addRandom = () => 1 + 2
+
+console.log(addWithBlock(3,5))
+console.log(add(3,5))
+console.log(addOne(3))
+console.log(addRandom())
+
+
+
