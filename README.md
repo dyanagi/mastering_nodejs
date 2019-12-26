a2b0f64f59767b2f31f03ba94e28017df20ba13a
Michael (Daichi) Yanagi <6637846+dyanagi@users.noreply.github.com>
2019-12-25 18:53:24 -0800

    Add README and comments

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mastering Node.js
+
+## Terms
+
+* REPL: Read the input, Evaluate it, Print the result, and Loop — Run `node` in terminal
+
--- first-app.js
+++ first-app.js
@@ -1,3 +1,5 @@
+// To run via command: `node first-app.js`
 const fs = require('fs')
 
+// Create a file
 fs.writeFileSync('hello.txt', 'Hello from Node.js')
